e6d71ab78f4d3f31f86777f63cea1cc75d72dc32
Josh Kroslowitz <joshkroz@gmail.com>
2022-09-27 18:59:53 +0800

    complete

---
+++ SVT_test/classes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace SVT_test
+{
+    public class Robot
+    {
+        public Robot() { }
+
+        [JsonPropertyName("robotId")]
+        public string? RobotId { get; set; }
+
+        [JsonPropertyName("batteryLevel")]
+        public int BatteryLevel { get; set; }
+
+        [JsonPropertyName("y")]
+        public int YCoord { get; set; }
+
+        [JsonPropertyName("x")]
+        public int XCoord { get; set; }
+    }
+
+    public class RobotData
+    {
+        public RobotData(string id, double dist, int batt)
+        {
+            RobotId = id;
+            DistanceToGoal = dist;
+            BatteryLevel = batt;
+        }
+
+        [JsonPropertyName("robotId")]
+        public string? RobotId { get; set; }
+
+        [JsonPropertyName("distanceToGoal")]
+        public double DistanceToGoal { get; set; }
+
+        [JsonPropertyName("batteryLevel")]
+        public int BatteryLevel { get; set; }
+    }
+
+    public class InputData
+    {
+        public InputData() { }
+        [JsonPropertyName("loadId")]
+        public string? LoadId { get; set; }
+
+        [JsonPropertyName("y")]
+        public int YCoord { get; set; }
+
+        [JsonPropertyName("x")]
+        public int XCoord { get; set; }
+    }
+}
